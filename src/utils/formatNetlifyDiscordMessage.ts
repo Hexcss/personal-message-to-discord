4baae450b14e6dfdcebace60a57a6860c40b7036
Hexcss <cadersuay.javier@gmail.com>
2023-08-09 00:28:36 +0200

    feat: Improved Discord Messages

--- src/utils/formatNetlifyDiscordMessage.ts
+++ src/utils/formatNetlifyDiscordMessage.ts
@@ -1,24 +1,44 @@
 import { NetlifyWebhookBody } from "../interfaces/NetlifyWebhookBody";
 
 export function formatNetlifyDiscordMessage(data: NetlifyWebhookBody): any {
+
+  const getStatusColor = (state: string) => {
+    switch (state) {
+      case 'deployed':
+        return 3066993; // Green
+      case 'error':
+        return 16711680; // Red
+      case 'building':
+        return 16753920; // Orange
+      default:
+        return 8421504; // Gray
+    }
+  };
+
   return {
     embeds: [
       {
-        title: "Netlify Deploy Notification",
-        description: `Site: [${data.name}](${data.site_url})`,
-        color: data.state === 'deployed' ? 3066993 : 15844367, // Green for deployed, Orange for other states
+        title: "Netlify Deploy Update",
+        description: `The site **[${data.name}](${data.site_url})** has been ${data.state}.`,
+        color: getStatusColor(data.state),
+        thumbnail: {
+          url: "https://www.netlify.com/img/press/logos/logomark.png" // Netlify logo
+        },
+        author: {
+          name: "Netlify",
+          icon_url: "https://www.netlify.com/img/press/logos/logomark.png",
+        },
         fields: [
           {
-            name: "Status",
-            value: data.state.charAt(0).toUpperCase() + data.state.slice(1), // Capitalize the status
-            inline: true,
-          },
-          {
-            name: "Context",
+            name: "Build Context",
             value: data.context,
             inline: true,
           }
         ],
+        footer: {
+          text: "Netlify Deploy Notifications",
+          icon_url: "https://www.netlify.com/img/press/logos/logomark.png"
+        },
         timestamp: new Date().toISOString(),
       },
     ],
