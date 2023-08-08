6f0ab8bdff5ed6cb074ddfb9e3df68d639c6a4b9
Hexcss <cadersuay.javier@gmail.com>
2023-08-08 08:57:35 +0200

    feat: Webhook URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const config = {
-  SLACK_WEBHOOK_URL: 'https://hooks.slack.com/services/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',
+  SLACK_WEBHOOK_URL: 'https://hooks.slack.com/services/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',
   PORT: process.env.PORT || 5000,
 };
