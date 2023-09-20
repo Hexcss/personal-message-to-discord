aa8244d6e43c697edc371b29101ba82f3cfd8bd2
Hexcss <cadersuay.javier@gmail.com>
2023-09-20 22:08:43 +0200

    fix: Env Variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv';
+import * as functions from 'firebase-functions';
 import { defineString } from 'firebase-functions/params';
 
 dotenv.config();
+functions.runWith({ secrets: ['DISCORD_GITHUB_WEBHOOK', 'DISCORD_NETLIFY_WEBHOOK'] })
 
 export const config = {
-  DISCORD_GITHUB_WEBHOOK: defineString("DISCORD_GITHUB_WEBHOOK ") as unknown as string || '',
-  DISCORD_NETLIFY_WEBHOOK: defineString("DISCORD_NETLIFY_WEBHOOK") as unknown as string || '',
+  DISCORD_GITHUB_WEBHOOK: process.env.DISCORD_GITHUB_WEBHOOK || '',
+  DISCORD_NETLIFY_WEBHOOK: process.env.DISCORD_NETLIFY_WEBHOOK || '',
 };
