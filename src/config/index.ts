731a39e46de54b51100c1dadd6bbe72a54fc1d03
Hexcss <cadersuay.javier@gmail.com>
2023-09-20 16:49:27 +0200

    feat: Fixed String Params

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,6 @@ import { defineString } from 'firebase-functions/params';
 dotenv.config();
 
 export const config = {
-  DISCORD_GITHUB_WEBHOOK: defineString("DISCORD_GITHUB_WEBHOOK ")|| '',
-  DISCORD_NETLIFY_WEBHOOK: defineString("DISCORD_NETLIFY_WEBHOOK")|| '',
+  DISCORD_GITHUB_WEBHOOK: defineString("DISCORD_GITHUB_WEBHOOK ") as unknown as string || '',
+  DISCORD_NETLIFY_WEBHOOK: defineString("DISCORD_NETLIFY_WEBHOOK") as unknown as string || '',
 };
